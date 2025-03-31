469e35a56371070a943e6dc2b9578e3d1bd36561
efrainguerra433 <efrainguerra433@gmail.com>
2025-03-31 13:16:51 -0500

    Edición configuración de flask

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 import os
 
-DB_USER = "johne"  # Cambia esto si usas otro usuario
+DB_USER = "johne" 
 DB_PASSWORD = "orange"
-DB_HOST = "localhost"  # Si es necesario, cambia por el nombre correcto de tu host
+DB_HOST = "localhost"  
 DB_NAME = "lionta_db"
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
